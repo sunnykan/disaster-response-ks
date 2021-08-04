f4e603f61f750edef3bf62b78ee89e71f3710826
K S <8283247+sunnykan@users.noreply.github.com>
2021-08-04 16:28:02 -0400

    Remove command to train model

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,1 @@
 python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
-python models/train_classifier.py data/DisasterResponse.db models/model_rfc_gridsearchcv.pkl
