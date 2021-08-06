1314182e0f63f3c70e66e62b0f10a378bdfef3d7
K S <8283247+sunnykan@users.noreply.github.com>
2021-08-06 13:57:01 -0400

    Edit pickle file name

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
-python models/train_classifier.py data/DisasterResponse.db models/model_rfc.pkl
+python models/train_classifier.py data/DisasterResponse.db models/model_logistic.pkl
