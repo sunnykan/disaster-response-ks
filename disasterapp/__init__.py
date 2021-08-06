0323b2da613d7a203f344adbe8ffa8d35440b78f
K S <8283247+sunnykan@users.noreply.github.com>
2021-08-06 07:55:36 -0400

    Move tokenize function into separate file

--- disasterapp/__init__.py
+++ disasterapp/__init__.py
@@ -2,4 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from disasterapp.utils import tokenize
 from disasterapp import routes
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,4 +1,3 @@
-from sqlalchemy import create_engine
 import pandas as pd
 import argparse
 from pathlib import Path
@@ -6,6 +5,9 @@ import re
 import numpy as np
 import json
 import dill
+
+from sqlalchemy import create_engine
+from typing import List, Tuple, Dict, Set
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -15,18 +17,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.corpus import words
-
-from typing import List, Tuple, Dict, Set
-
-# create a set of English words using a corpus
-stop_words: List = stopwords.words("english")
-vocab: Set = set(words.words()) - set(stop_words)
-lemmatizer = WordNetLemmatizer()
-words_set: set = {lemmatizer.lemmatize(word) for word in vocab}
+from disasterapp import tokenize
 
 
 def load_data(database_filepath: str) -> Tuple[np.ndarray, np.ndarray, List]:
@@ -47,25 +38,6 @@ def load_data(database_filepath: str) -> Tuple[np.ndarray, np.ndarray, List]:
     output_labels = list(df.iloc[:, :-3].columns)
 
     return X, Y, output_labels
-
-
-def tokenize(text: str) -> List:
-    """
-    Tokenize and lemmatize text
-
-    :param text: Text to be tokenized
-    :return: List of lemmatized word tokens
-    """
-
-    text = re.sub(r"[^a-zA-Z]", " ", text.lower())
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = [
-        lemmatizer.lemmatize(token).strip() for token in tokens if token in words_set
-    ]
-
-    return clean_tokens
 
 
 def build_model() -> Pipeline:
