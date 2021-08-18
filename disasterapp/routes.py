f1dacea2a75c6e34069ec71464f08a14259f822d
K S <8283247+sunnykan@users.noreply.github.com>
2021-08-18 14:42:06 -0400

    Change reference to utils.py

--- disasterapp/routes.py
+++ disasterapp/routes.py
@@ -8,8 +8,10 @@ import joblib
 from flask import render_template, request, jsonify
 import plotly.graph_objects as gro
 from sqlalchemy import create_engine
-from utils import tokenize
+
+# from utils import tokenize
 from typing import List
+from disasterapp.utils import tokenize
 
 
 # load data
